45b85947baafae20e26a4c0d04a736b5d8065139
Tony DiNitto <think41c@gmail.com>
2015-09-22 23:45:22 -0500

    Can scan through a word and find a matching letter.

--- letter_finder.rb
+++ letter_finder.rb
@@ -1,8 +1,17 @@
 class Finder
-  def what_index(starting_point, word)
-  
+  def what_index(word, letter, start_index)
+    string_length = word.length
+    puts string_length
+    (0..string_length-1).each do |x|
+      puts word[x]
+      if word[x] == letter
+        puts "We got a hit"
+      end
+      
+    end
+
   end
 end
 
 a = Finder.new
-a.what_index(4, "apple")
+a.what_index("apple", "p", 1)
