a0299902b0fa6da9fe8e84ff304f4783b1af3283
Tony DiNitto <think41c@gmail.com>
2015-09-22 23:49:58 -0500

    Works perfectly and finds letter fine.

--- letter_finder.rb
+++ letter_finder.rb
@@ -1,17 +1,20 @@
 class Finder
   def what_index(word, letter, start_index)
+    word   = word.downcase
+    letter = letter.downcase
     string_length = word.length
-    puts string_length
     (0..string_length-1).each do |x|
-      puts word[x]
       if word[x] == letter
-        puts "We got a hit"
+        return x + 1
+      elsif x == string_length-1
+        return -1
       end
-      
+
     end
 
   end
 end
 
 a = Finder.new
-a.what_index("apple", "p", 1)
+p a.what_index("apple", "z", 1)
+p a.what_index("azzpple", "z", 1)
